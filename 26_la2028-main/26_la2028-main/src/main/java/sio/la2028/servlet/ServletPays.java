46cd9ebe5269f3a1d304cb0ea72f9191e7691c73
NathanElie27 <nathanelie.06@gmail.com>
2026-09-16 10:39:49 +0200

    feat, creation lister pays, ServletPays

---
+++ 26_la2028-main/26_la2028-main/src/main/java/sio/la2028/servlet/ServletPays.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package sio.la2028.servlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import sio.la2028.database.DaoSport;
+import sio.la2028.database.DaoPays;
+import sio.la2028.database.DaoSport;
+import sio.la2028.model.Sport;
+import sio.la2028.model.Pays;
+import sio.la2028.model.Sport;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author zakina
+ */
+public class ServletPays extends HttpServlet {
+
+    Connection cnx ;
+
+    @Override
+    public void init()
+    {
+        ServletContext servletContext=getServletContext();
+
+        System.out.println("SERVLKET CONTEXT=" + servletContext.getContextPath());
+        cnx = (Connection)servletContext.getAttribute("connection");
+
+        try {
+            System.out.println("INIT SERVLET=" + cnx.getSchema());
+        } catch (SQLException ex) {
+            Logger.getLogger(ServletPays.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletPays</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletPays at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String url = request.getRequestURI();
+
+        // Récup et affichage les Pays
+        if(url.equals("/la2028/ServletPays/lister"))
+        {
+            ArrayList<Pays> lesPays = DaoPays.getLesPays(cnx);
+            request.setAttribute("pLesPays", lesPays);
+            //System.out.println("lister eleves - nombres d'élèves récupérés" + lesEleves.size() );
+            getServletContext().getRequestDispatcher("/vues/Pays/listerPays.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

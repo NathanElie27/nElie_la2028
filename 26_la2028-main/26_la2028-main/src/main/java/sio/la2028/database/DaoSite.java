9881142054e9564199eb46880333167c95679091
NathanElie27 <nathanelie.06@gmail.com>
2026-09-21 16:04:42 +0200

    feat, ajout servletSite

--- 26_la2028-main/26_la2028-main/src/main/java/sio/la2028/database/DaoSite.java
+++ 26_la2028-main/26_la2028-main/src/main/java/sio/la2028/database/DaoSite.java
@@ -1,4 +1,43 @@
 package sio.la2028.database;
 
+import sio.la2028.model.Site;
+import sio.la2028.model.Sport;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class DaoSite {
+
+    Connection cnx;
+    static PreparedStatement requeteSql = null;
+    static ResultSet resultatRequete = null;
+
+    public static ArrayList<Site> getLesSites(Connection cnx){
+
+        ArrayList<Site> lesSites = new ArrayList<Site>();
+        try{
+            requeteSql = cnx.prepareStatement("select * from sports;");
+            //System.out.println("REQ="+ requeteSql);
+            resultatRequete = requeteSql.executeQuery();
+
+            while (resultatRequete.next()){
+
+                Site s = new Site();
+                s.setId(resultatRequete.getInt("s_id"));
+                s.setNom(resultatRequete.getString("s_nom"));
+
+                lesSites.add(s);
+            }
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("La requête de getLespayss e généré une erreur");
+        }
+        return lesSites;
+
+    }
 }

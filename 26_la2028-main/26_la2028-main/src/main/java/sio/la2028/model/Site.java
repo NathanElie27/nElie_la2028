91ca3ce5f120140dcf6a919677a03b903ac659fd
NathanElie27 <nathanelie.06@gmail.com>
2026-09-21 15:43:17 +0200

    feat creation model site

---
+++ 26_la2028-main/26_la2028-main/src/main/java/sio/la2028/model/Site.java
@@ -0,0 +1,26 @@
+package sio.la2028.model;
+
+public class Site {
+
+    private int id;
+    private String nom;
+
+    public Site() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
